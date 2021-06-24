49daace83a6bc6e1e388a8c750277d6152d8ea07
Fanchen Bao <jonathonbao@gmail.com>
2021-06-24 18:41:35 -0400

    Set up initial file structure

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+// Import types
+import type {Node} from 'react';
+
+export const HomeScreen = (): Node => {
+  return (
+    <View>
+      <Text>Hellow World</Text>
+    </View>
+  );
+};
